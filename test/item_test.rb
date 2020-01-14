7cf58221445765540469b46efed557db5e341ce8
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-14 09:09:55 -0700

    Add files and frames for vendor and item tests

---
+++ test/item_test.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/item'
+
+class ItemTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/vendor'
+require_relative '../lib/item'
+
+class VendorTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_
+end
