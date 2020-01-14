97e6ca3b1ad6b8757e132b817f4bf9eeede49c6d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-14 09:17:40 -0700

    Add tests for vendor for attributes and existence

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,10 +5,22 @@ require_relative '../lib/item'
 
 class VendorTest < Minitest::Test
   def setup
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item1 = Item.new({name: 'Peach', price: '$0.75'})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor = Vendor.new('Rocky Mountain Fresh')
   end
 
-  def test_
+  def test_vendor_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_vendor_has_attributes
+    assert_equal 'Rocky Mountain Fresh', @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_stock_defaults_zero
+    assert_equal 0, @vendor.check_stock(@item1)
+    assert_equal 0, @vendor.check_stock(@item2)
+  end
 end
